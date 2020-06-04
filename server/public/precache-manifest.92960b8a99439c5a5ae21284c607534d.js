088d817f1d0fb84def5d0136f24cefeec77674b7
Assaf Shibber <036190536@idf.il>
2020-06-04 20:18:23 +0300

    upload public with new postbuild command

---
+++ server/public/precache-manifest.92960b8a99439c5a5ae21284c607534d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913cac8c066674e7ae8ba060b3b96d49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "153ad5959d525627b096",
+    "url": "/static/css/main.5dcec62c.chunk.css"
+  },
+  {
+    "revision": "cefdcea059e75567fd72",
+    "url": "/static/js/2.2e4dbc02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2e4dbc02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "153ad5959d525627b096",
+    "url": "/static/js/main.392473f5.chunk.js"
+  },
+  {
+    "revision": "493aaee7ee8c5de84d5e",
+    "url": "/static/js/runtime-main.a59e3382.js"
+  }
+]);
